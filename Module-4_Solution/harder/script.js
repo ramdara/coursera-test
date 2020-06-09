1554663ca7cde12670e6ea8f7a1bbc6dc35ca977
ramdara <52502904+ramdara@users.noreply.github.com>
2020-06-09 17:11:46 +0530

    small changes

--- Module-4_Solution/harder/script.js
+++ Module-4_Solution/harder/script.js
@@ -1,8 +1,8 @@
 (function(window){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i=0;i<names.length;i++) {
-  var firstLetter = names[i][0];
-  if (firstLetter=='j' || firstLetter=='J') {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter==='j') {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
